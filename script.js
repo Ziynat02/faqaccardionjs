8e54eb7d44589ad15d21473c28d93b4ab7b8623b
Ziynat02 <ziynatimomqulova02@gmail.com>
2023-10-24 14:25:41 +0500

    faq

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const btn=document.querySelectorAll(".btn");
+
+btn.forEach(function(button){
+  button.addEventListener("click",function(){
+    this.classList.toggle("active");
+
+
+    if(button.classList.contains("active")){
+      btn.forEach(function(button){
+        button.classList.remove("active");
+      });
+      button.classList.toggle("active");
+    }
+  });
+});
+
